79ab2e3f16c47873cfcafe63e1cffff49bfb68d3
Edrian <mr.edrian@gmail.com>
2022-10-20 12:35:23 +0800

    First Commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+
+    public function index()
+    {
+        $category = Category::all();
+        return response()->json([
+            'status' => 200,
+            'category' => $category,
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        if ($category) {
+            return response()->json([
+                'status' => 200,
+                'category' => $category
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => "No Category Id Found"
+            ]);
+        }
+    }
+
+
+
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'meta_title' => 'required|max:191',
+            'slug' => 'required|max:191',
+            'name' => 'required|max:191',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->messages(),
+            ]);
+        } else {
+
+            $category = new Category;
+            $category->meta_title = $request->input('meta_title');
+            $category->meta_keyword = $request->input('meta_keyword');
+            $category->meta_descrip = $request->input('meta_descrip');
+            $category->slug = $request->input('slug');
+            $category->name = $request->input('name');
+            $category->description = $request->input('description');
+            $category->status = $request->input('status') == true ? '1' : '0';
+            $category->save();
+            return response()->json([
+                'status' => 200,
+                'message' => "Category Added Succesfully",
+            ]);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'meta_title' => 'required|max:191',
+            'slug' => 'required|max:191',
+            'name' => 'required|max:191',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages(),
+            ]);
+        } else {
+
+            $category = Category::find($id);
+            if ($category) {
+                $category->meta_title = $request->input('meta_title');
+                $category->meta_keyword = $request->input('meta_keyword');
+                $category->meta_descrip = $request->input('meta_descrip');
+                $category->slug = $request->input('slug');
+                $category->name = $request->input('name');
+                $category->description = $request->input('description');
+                $category->status = $request->input('status') == true ? '1' : '0';
+                $category->save();
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Update Success",
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'No Category Id Found',
+                ]);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+        if ($category) {
+            $category->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Category Successfully Deleted',
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Category Id Found',
+            ]);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum', 'isAPIAdmin'])->group(function () {
+    Route::get('/checkingAuthenticated', function () {
+        return response()->json(['message' => 'You are in', 'status' => 200], 200);
+    });
+
+    //category
+    Route::get('view-category', [CategoryController::class, 'index']);
+    Route::post('store-category', [CategoryController::class, 'store']);
+    Route::get('edit-category/{id}', [CategoryController::class, 'edit']);
+    Route::put('update-category/{id}', [CategoryController::class, 'update']);
+    Route::delete('delete-category/{id}', [CategoryController::class, 'destroy']);
+});
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
